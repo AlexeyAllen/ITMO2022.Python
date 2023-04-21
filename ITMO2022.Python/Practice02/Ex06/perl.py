9afdcee760d82cdbebb8d75508deb905078318af
Aleksei Allen <aqml@mail.ru>
2023-04-21 20:17:53 +0500

    Practice02: excercise 06 def read_zip_all() added to perl.py to improve the program start up process

--- ITMO2022.Python/Practice02/Ex06/perl.py
+++ ITMO2022.Python/Practice02/Ex06/perl.py
@@ -1,8 +1,5 @@
-from Practice02.Ex06.zip_util import read_zip_all
+# from Practice02.Ex06.zip_util import read_zip_all
 from math import sin, cos, sqrt, atan2, radians
-
-zip_codes = read_zip_all()
-
 
 def deg_lat_convert(deg):
     m, s = divmod(abs(deg) * 3600, 60)
@@ -64,6 +61,36 @@ def get_distance(zip1, zip2):
     distance = earth_rad_miles * c
     return f'{distance:.2f}'
 
+def read_zip_all():
+    i = 0
+    header = []
+    zip_codes = []
+    zip_data = []
+    skip_line = False
+    # http://notebook.gaslampmedia.com/wp-content/uploads/2013/08/zip_codes_states.csv
+    for line in open('zip_codes_states.csv').read().split("\n"):
+        skip_line = False
+        m = line.strip().replace('"', '').split(",")
+        i += 1
+        if i == 1:
+            for val in m:
+                header.append(val)
+        else:
+            zip_data = []
+            for idx in range(0,len(m)):
+                if m[idx] == '':
+                    skip_line = True
+                    break
+                if header[idx] == "latitude" or header[idx] == "longitude":
+                    val = float(m[idx])
+                else:
+                    val = m[idx]
+                zip_data.append(val)
+            if not skip_line:
+                zip_codes.append(zip_data)
+    return zip_codes
+
+zip_codes = read_zip_all()
 
 def main():
     while True:
